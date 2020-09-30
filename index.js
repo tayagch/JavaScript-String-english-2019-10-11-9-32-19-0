61b91fd75a84e5bfc77441235ae134f236c34bca
CHRISTIAN MICHAEL TAYAG (PLAN-IRIS-ISD-OOCLL/MNL) <TAYAGCH@OOCL.COM>
2020-09-30 13:20:26 +0800

    submit answer

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+console.log('\''+name.toUpperCase() + '\'');
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+var tempSentence = sentence.split(' ');
+var result = "";
+for(var i = 0; i < tempSentence.length; i++){
+    result += tempSentence[i].charAt(0).toUpperCase() + tempSentence[i].substring(1) + ' '
+}
+console.log(result);
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+console.log(money.substr(1,2));
